00730ca38c2b957835fad25cd9064d31e0b7d856
Shrestha-Aaron <official.aaron.np@gmail.com>
2026-01-04 18:33:02 +0545

    Update Analyzer.py
    added result finder function that returns  a tuple containing result as percentage and gpa. Also incomplete definition of division finder.

--- Student-performance-Analyzer/Analyzer.py
+++ Student-performance-Analyzer/Analyzer.py
@@ -3,3 +3,15 @@ try:
   data=pd.read_csv('students.csv')
 except exception:
   print('File not found.')
+
+#Result
+def result(*subjects):
+  percentage=sum(subjects)
+  gpa=percentage/25
+  return (percentage, gpa)
+
+#Finds division, expects result in percentage
+def DivisionFinder(x): 
+  '''Expects result in percentage'''
+  pass
+
